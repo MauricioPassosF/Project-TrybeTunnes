91a0f70071bbdd7d1d01500acf13757c11cfe97a
MauricioPassosF <mauricio.passos97@gmail.com>
2023-04-13 16:19:40 -0300

    Requisito 1

---
+++ src/pages/ProfileEdit.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProfileEdit extends Component {
+  render() {
+    return (
+      <div data-testid="page-profile-edit">
+        Teste ProfileEdit
+      </div>
+    );
+  }
+}
+
+export default ProfileEdit;
