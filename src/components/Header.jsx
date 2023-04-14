cba61219112cfee9ee343827e9fb729432b93861
MauricioPassosF <mauricio.passos97@gmail.com>
2023-04-14 11:07:45 -0300

    Requisito 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 
 class Header extends Component {
@@ -6,14 +7,10 @@ class Header extends Component {
     userName: '',
   };
 
-  componentDidMount() {
-    this.getUserInfo();
-  }
-
-  getUserInfo = async () => {
+  async componentDidMount() {
     const userInfo = await getUser();
     this.setState({ userName: userInfo.name });
-  };
+  }
 
   render() {
     const { userName } = this.state;
@@ -24,6 +21,9 @@ class Header extends Component {
             <>
               <h1>TrybeTunes</h1>
               <h3 data-testid="header-user-name">{userName}</h3>
+              <Link to="/search" data-testid="link-to-search">Search</Link>
+              <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+              <Link to="/profile" data-testid="link-to-profile">Profile</Link>
             </>
           ) : (
             <p>Carregando...</p>
