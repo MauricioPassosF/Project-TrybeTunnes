362c69bca9794d9866d134946647af9ead5ea2fb
MauricioPassosF <mauricio.passos97@gmail.com>
2023-04-14 11:25:56 -0300

    Requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,43 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state = {
+    inputArtist: '',
+  };
+
+  handlerInput = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
+    const { inputArtist } = this.state;
+    const minArtistLength = 2;
+
     return (
       <div data-testid="page-search">
         <Header />
         Teste Search
+        <label htmlFor="inputArtist">
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            placeholder="Artista desejado"
+            name="inputArtist"
+            id="inputArtist"
+            onChange={ this.handlerInput }
+            value={ inputArtist }
+          />
+        </label>
+        <button
+          data-testid="search-artist-button"
+          disabled={ (inputArtist.length < minArtistLength) }
+          // onClick={ () => this.handlerButton(history) }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
